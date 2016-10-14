8a6e943b51edb482d39883a7c176df277a6d5784
giscafer <giscafer@outlook.com>
2016-10-14 22:59:13 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+/**
+ * app entry
+ */
+'use strict'
+
+const path=require('path');
+const express=require('express');
+const bodyParser=require('body-parser');
+const Typhoon=require('node-typhoon');
+const app=express();
+
+app.set('port',(process.env.PORT || 3000));
+
+app.use('',express.static(path.join(__dirname,'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+//get real-time typhoon information 
+app.get('/api/typhoonActivity',(req,res)=>{
+	Typhoon.typhoonActivity().then(info=>{
+		res.send({
+			status:200,
+			data:info,
+			message:'request success!'
+		});
+	}).catch(err=>{
+		console.error({
+			status:501,
+			error:err,
+			message:'request failure!'
+		});
+	});
+});
+//get historical typhoon information 
+app.get('/api/typhoonList',(req,res)=>{
+	let year=new Date().getFullYear();
+	Typhoon.typhoonList(year).then(info=>{
+		res.send({
+			status:200,
+			data:info,
+			message:'request success!'
+		});
+	}).catch(err=>{
+		console.error({
+			status:501,
+			error:err,
+			message:'request failure!'
+		});
+	});
+});
+
+app.listen(app.get('port'),()=>{
+	console.log('Server started: http://localhost:'+app.get('port')+'/');
+});
---
+++ public/scripts/map.js
@@ -0,0 +1,140 @@
+ var map;
+ var SERVER_URL = "http://localhost:3000/";
+ var typhoonCenterPnt = [23.18, 113.24];
+
+ function init() {
+     map = WE.map('map', {
+         center: [23.08, 113.14],
+         zoom: 3,
+         dragging: true,
+         scrollWheelZoom: true,
+         proxyHost: 'http://srtm.webglearth.com/cgi-bin/corsproxy.fcgi?url='
+     });
+
+     var baselayer = WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         attribution: '© OpenStreetMap contributors'
+     }).addTo(map);
+
+     //Add TileJSON layer
+     var json = { "profile": "mercator", "name": "Grand Canyon USGS", "format": "png", "bounds": [-112.26379395, 35.98245136, -112.10998535, 36.13343831], "minzoom": 10, "version": "1.0.0", "maxzoom": 16, "center": [-112.18688965, 36.057944835, 13], "type": "overlay", "description": "", "basename": "grandcanyon", "tilejson": "2.0.0", "sheme": "xyz", "tiles": ["http://tileserver.maptiler.com/grandcanyon/{z}/{x}/{y}.png"] };
+     var grandcanyon = WE.tileLayerJSON(json);
+     grandcanyon.addTo(map);
+
+     grandcanyon.setOpacity(0.7);
+     document.getElementById('opacity2').addEventListener('change', function(e) {
+         grandcanyon.setOpacity(e.target.value);
+     });
+
+
+     //Print coordinates of the mouse
+     map.on('mousemove', function(e) {
+         if (e.latlng) {
+             document.getElementById('coords').innerHTML = e.latlng.lat + ', ' + e.latlng.lng;
+         }
+     });
+     addRealTimeTyphoonMarker(2);
+ }
+
+ function addRealTimeTyphoonMarker(level) {
+     if (!level) {
+         level = 5;
+     }
+     document.getElementById('realtime_typhoon').disabled = true;
+     document.getElementById('historical_typhoon').disabled = false;
+     document.getElementById('realtime_center').disabled = false;
+
+     $.get(SERVER_URL + "api/typhoonActivity", function(res) {
+         if (res.status === 200) {
+             var data = JSON.parse(res.data);
+             console.info(data, level);
+             addMarkers(data);
+             if (data.length > 0) {
+                 var lat = data[0]['lat'] - 0,
+                     lng = data[0]['lng'] - 0;
+                 window.typhoonCenterPnt = [lat, lng];
+             } else {
+                 window.typhoonCenterPnt = [23.18, 113.24];
+                 alert('there is no typhoon！');
+             }
+
+
+         } else {
+             alert('Server error!!!');
+         }
+     });
+ }
+ /**
+  * 历史列表
+  * @author giscafer
+  * @version 1.0
+  * @date    2016-10-14T22:42:56+0800
+  */
+ function addHistoricalTyphoonMarker() {
+     document.getElementById('realtime_center').disabled = true;
+     document.getElementById('historical_typhoon').disabled = true;
+     document.getElementById('realtime_typhoon').disabled = false;
+     $.get(SERVER_URL + "api/typhoonList", function(res) {
+         if (res.status === 200) {
+             var data = JSON.parse(res.data);
+             // addMarkers(data);
+         } else {
+             alert('Server error!!!');
+         }
+     });
+ }
+
+ function addMarkers(data,level) {
+     if (!data) return;
+     console.log(data);
+     for (var i = 0; i < data.length; i++) {
+         var info = data[i],
+             lat = info['lat'] - 0,
+             lng = info['lng'] - 0,
+             point = [lat, lng];
+         var title = info['enname'] + '（' + name + '）',
+             power = info['power'] || '',
+             speed = info['speed'] || '',
+             strong = info['strong'] || '',
+             movedirection = info['movedirection'] || '',
+             movespeed = info['movespeed'] || '',
+             pressure = info['pressure'] || '',
+             radius7 = info['radius7'] || '',
+             radius10 = info['radius10'] || '',
+             tfid = info['tfid'] || '',
+             timeformate = info['timeformate'] || '',
+             pos = '纬度：' + lat + ',经度：' + lng;
+         var templateHtml = '<div><b>' + tfid + '号台风，' + timeformate + '</b><div>' +
+             '<br>当前位置 <b>' + pos + '</b>' +
+             '<br>中心气压 <b>' + pressure + '</b> 百帕' +
+             '<br>最大风速 <b>' + speed + '</b> 米/秒' +
+             '<br>风 力 <b>' + power + '</b> 级' +
+             '<br>等 级 <b>' + strong + '</b> ' +
+             '<br>移动速度<b>' + movespeed + '</b> 公里/小时' +
+             '<br>移动方向<b>' + movedirection + '</b> ' +
+             '<br>七级半径<b>' + radius7 + '</b> 公里' +
+             '<br>十级半径<b>' + radius10 + '</b> 公里';
+         var marker = WE.marker(point,'./images/typhoon.png',40).addTo(map);
+         marker.bindPopup(templateHtml, { maxWidth: 150, closeButton: true }).openPopup();
+     }
+     map.setView(typhoonCenterPnt, level);
+ }
+
+ function setZoom(zoom) {
+     map.setZoom(zoom);
+ }
+
+ function setPositionToEverest() {
+     map.setView([27.988056, 86.925278]);
+ }
+
+ function flyToTyphoonCenter() {
+     map.panTo([23.08, 113.14]);
+ }
+ // center to GuangZhou China
+ function resetZoom() {
+     map.panTo([23.08, 113.14]);
+ }
+
+ function starOnGithub() {
+     window.open('https://github.com/viseye/webglearth-typhoon');
+ }
